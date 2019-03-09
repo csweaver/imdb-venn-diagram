eb4baf1c30fd41c51c1f50f1d04d0a5f91db1532
Charlotte Weaver <charlottesweaver@gmail.com>
2019-03-09 15:19:01 -0800

    Restructure client  directory

---
+++ client/src/actions/index.js
@@ -0,0 +1,72 @@
+const search_url = "http://localhost:5000/api/search";
+const venn_url = "http://localhost:5000/api/venn";
+
+const recieveSearch = json => ({
+  type: "got movie search results",
+  movies: json.possibilities,
+  receivedAt: Date.now()
+});
+
+export const doSearch = movie_string => {
+  return dispatch => {
+    console.log(`${search_url}?title=${movie_string}`);
+    return fetch(`${search_url}?title=${movie_string}`, {
+      credentials: "same-origin"
+    })
+      .then(response => {
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error("Uhhh... IDK? No  movie");
+        }
+        return response.json();
+      })
+      .then(json => {
+        dispatch(recieveSearch(json));
+      })
+      .catch(e => {
+        console.log(e);
+        console.log("something went wrong");
+      });
+  };
+};
+
+export const addSelection = movie_id => ({
+  type: "add movie to selection",
+  selected: movie_id
+});
+
+export const deleteSelection = movie_id => ({
+  type: "remove movie from selection",
+  deselected: movie_id
+});
+
+export const doVenn = movie_list => {
+  return dispatch => {
+    let movie_params = movie_list.map(movie => `movies=${movie}`);
+    movie_params = movie_params.join("&");
+    return fetch(`${venn_url}?${movie_params}`, { credentials: "same-origin" })
+      .then(response => {
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error("Uhhh... IDK? No  movie");
+        }
+        return response.json();
+      })
+      .then(json => {
+        dispatch(recieveVenn(json));
+      })
+      .catch(e => {
+        console.log(e);
+        console.log("something went wrong");
+      });
+  };
+};
+
+const recieveVenn = json => ({
+  type: "got venn results",
+  results: json,
+  receivedAt: Date.now()
+});
+
+export const clearUnselected = movie_list => ({
+  type: "clear unselected",
+  currently_selected: movie_list
+});
---
+++ client/src/reducers/index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by weaverc10 on 2/16/19.
+ */
+import { combineReducers } from "redux";
+
+const basic = (state = {}, action) => {
+  if (action.type === "basic type") {
+    return {
+      ...state
+    };
+  } else {
+    return state;
+  }
+};
+
+const search_results = (state = { movies: [] }, action) => {
+  if (action.type === "got movie search results") {
+    let movies = state.movies;
+    movies = movies.concat(action.movies);
+    return {
+      ...state,
+      movies
+    };
+  } else if (action.type === "clear unselected") {
+    const movies = state.movies.filter(movie => {
+      return action.currently_selected.includes(movie.id);
+    });
+    return {
+      ...state,
+      movies: movies
+    };
+  } else {
+    return state;
+  }
+};
+
+const selected_movies = (state = { selected: [] }, action) => {
+  if (action.type === "add movie to selection") {
+    let selected = state.selected;
+    selected.push(action.selected);
+    return {
+      ...state,
+      selected
+    };
+  } else if (action.type === "remove movie from selection") {
+    let selected = state.selected;
+    selected = selected.filter(val => {
+      return val !== action.deselected;
+    });
+    return {
+      ...state,
+      selected
+    };
+  } else {
+    return state;
+  }
+};
+
+const overlap = (state = { overlap: { movies: [], actors: [] } }, action) => {
+  if (action.type === "got venn results") {
+    return {
+      ...state,
+      overlap: action.results
+    };
+  } else {
+    return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  basic,
+  search_results,
+  selected_movies,
+  overlap
+});
